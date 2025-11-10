0ee6d9890b52fc7faefc2505721f57f04f628ee2
NANDINEE RAJ <technonr2307@gmail.com>
2025-11-10 21:37:52 +0530

    broadcasting in numpy

--- NUMPYYY/multidim_array.py
+++ NUMPYYY/multidim_array.py
@@ -7,3 +7,7 @@ print(arr.shape)
 print(arr.size)
 # multidim indexing faster than chain indexing
 print(arr[1,1])
+word=arr[0,1]+arr[0,0]+arr[1,0]
+wordd=arr[1,0]+arr[0,0]+arr[1,0]
+print(f"if you are {word}, then I am your {wordd}")
+
